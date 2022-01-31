015f0c91ce5edc20216db9bf42e1c0da3cb986b4
Mubashir-M-Ibrahim <mubashirm.ibrahim@gmail.com>
2022-02-01 00:37:20 +0500

    react_bio_data_project

--- src/index.js
+++ src/index.js
@@ -5,11 +5,45 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+
+<div>
+<h1 className='hello'>bio data</h1>
+
+<div className='main'>
+
+<div className='ques'>
+
+<h2 className='name center'>Name :</h2>
+<h2 className='name center'>F.Name :</h2>
+<h2 className='name center'>Date Of Birth :</h2>
+<h2 className='name center'>Gender :</h2>
+<h2 className='name center'>Martial Status :</h2>
+</div>
+
+<div className='ans'>
+
+<h2 className='name center fullname'>Mubashir</h2>
+<h2 className='name center fullname'>Ibrahim</h2>
+<h2 className='name center'>29/01/2002</h2>
+<h2 className='name center'>Male</h2>
+<h2 className='name center'>Single</h2>
+</div>
+
+</div>
+
+</div>,
+
+
   document.getElementById('root')
 );
+
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
